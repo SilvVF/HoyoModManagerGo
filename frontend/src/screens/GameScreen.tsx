f5b6dd17e9af86cdd53e48c30497f3e7335fbcc9
SilvVF <david.silver232@gmail.com>
2024-09-03 05:23:42 -0400

    update game screen ui

--- frontend/src/screens/GameScreen.tsx
+++ frontend/src/screens/GameScreen.tsx
@@ -5,6 +5,8 @@ import { useStateProducer } from "../lib/utils"
 import { types } from "wailsjs/go/models"
 import { EnableModById } from "../../wailsjs/go/core/DbHelper"
 import { Switch } from "@/components/ui/switch"
+import { Card } from "@/components/ui/card"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 
 function GameScreen(props: {dataApi: DataApi}) {
@@ -25,7 +27,7 @@ function GameScreen(props: {dataApi: DataApi}) {
     }, [props.dataApi, refreshTrigger])
   
     return (
-        <div className="grid grid-cols-4">
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
           {
             characters.map((c) => (
               <div className="col-span-1">
@@ -42,22 +44,26 @@ function CharacterBox({ cmt, enableMod  }: { cmt: types.CharacterWithModsAndTags
   const character: types.Character = cmt.characters
 
   return (
-    <div className="aspect-square">
-      <img src={character.avatarUrl}></img>
-      <b>{character.name}</b>
-      {
-        cmt.mods.map((mod) => {
-          return (
-            <div className="flex flex-row" key={mod.id}>
-              <b>{mod.filename}</b>
-              <Switch checked={mod.enabled} onCheckedChange={() => enableMod(mod.id, !mod.enabled)} />
-            </div>
-          )
-        })
-      }
-      <b>{cmt.mods.toString()}</b>
-      <b>{cmt.tags.toString()}</b>
-    </div>
+    <Card className="m-2">
+      <div className="flex flex-row  m-2">
+        <div className="flex flex-col items-start">
+          <img src={character.avatarUrl}></img>
+          <b className="text-lg p-2">{character.name}</b>
+        </div>
+        <ScrollArea className="flex flex-col max-h-[300px] w-full">
+        {
+          cmt.mods.map((mod) => {
+            return (
+              <div className="flex flex-row justify-between m-2" key={mod.id}>
+                <b className="text-sm pe-2">{mod.filename}</b>
+                <Switch checked={mod.enabled} onCheckedChange={() => enableMod(mod.id, !mod.enabled)} />
+              </div>
+            )
+          })
+        }
+        </ScrollArea>
+      </div>
+    </Card>
   ) 
 }
 
