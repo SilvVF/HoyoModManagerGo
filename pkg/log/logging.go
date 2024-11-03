49ff0c5b29051802102cb46ed2b4ac7885ef8fd1
SilvVF <david.silver232@gmail.com>
2024-11-03 16:30:20 -0500

    add format string log options

--- pkg/log/logging.go
+++ pkg/log/logging.go
@@ -15,12 +15,28 @@ func InitLogging(ctx context.Context) {
 	initialized = true
 }
 
+func LogErrorf(format string, args ...any) {
+	if !initialized {
+		log.Printf(format, args...)
+		return
+	}
+	runtime.LogErrorf(logCtx, format, args...)
+}
+
 func LogError(message string) {
 	if !initialized {
-		log.Print(message)
+		log.Println(message)
 		return
 	}
 	runtime.LogError(logCtx, message)
+}
+
+func LogDebugf(format string, args ...any) {
+	if !initialized {
+		log.Printf(format, args...)
+		return
+	}
+	runtime.LogDebugf(logCtx, format, args...)
 }
 
 func LogDebug(message string) {
@@ -31,12 +47,28 @@ func LogDebug(message string) {
 	runtime.LogDebug(logCtx, message)
 }
 
+func LogFatalf(format string, args ...any) {
+	if !initialized {
+		log.Fatalf(format, args...)
+		return
+	}
+	runtime.LogFatalf(logCtx, format, args...)
+}
+
 func LogFatal(message string) {
 	if !initialized {
-		log.Println(message)
+		log.Fatalln(message)
 		return
 	}
 	runtime.LogFatal(logCtx, message)
+}
+
+func LogPrintf(format string, args ...any) {
+	if !initialized {
+		log.Printf(format, args...)
+		return
+	}
+	runtime.LogPrintf(logCtx, format, args...)
 }
 
 func LogPrint(message string) {
