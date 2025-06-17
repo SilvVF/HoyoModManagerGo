10875926d78557122ea4ace6325875bc3e6bd382
SilvVF <david.silver232@gmail.com>
2025-06-17 13:24:29 -0400

    fix delete mod broadcasting wrong key.

--- app/frontend/src/data/database.ts
+++ app/frontend/src/data/database.ts
@@ -1,144 +1,206 @@
 import { CancelFn } from "@/lib/tsutils";
 import { useEffect } from "react";
-import { CreateCustomCharacter, CreatePlaylist, DeleteCharacter, DeleteModById, DeletePlaylistById, DeleteTag, DeleteTextureById, InsertTag, InsertTagForAllModsByCharacterIds, RenameMod, RenameTexture, SelectCharactersByGame, SelectCharacterWithModsTagsAndTextures, SelectClosestCharacter, SelectModById, SelectModsByCharacterName, SelectModsByGbId, SelectPlaylistWithModsAndTags, SelectTagsByModId, UpdateDisableAllModsByGame, UpdateModEnabledById, UpdateModGbId, UpdateModImages, UpdateModsEnabledFromSlice, UpdatePlaylistName, UpdateTextureEnabledById } from "wailsjs/go/dbh/DbHelper";
+import {
+  CreateCustomCharacter,
+  CreatePlaylist,
+  DeleteCharacter,
+  DeleteModById,
+  DeletePlaylistById,
+  DeleteTag,
+  DeleteTextureById,
+  InsertTag,
+  InsertTagForAllModsByCharacterIds,
+  RenameMod,
+  RenameTexture,
+  SelectCharactersByGame,
+  SelectCharacterWithModsTagsAndTextures,
+  SelectClosestCharacter,
+  SelectModById,
+  SelectModsByCharacterName,
+  SelectModsByGbId,
+  SelectPlaylistWithModsAndTags,
+  SelectTagsByModId,
+  UpdateDisableAllModsByGame,
+  UpdateModEnabledById,
+  UpdateModGbId,
+  UpdateModImages,
+  UpdateModsEnabledFromSlice,
+  UpdatePlaylistName,
+  UpdateTextureEnabledById,
+} from "wailsjs/go/dbh/DbHelper";
 import { SplitTexture } from "wailsjs/go/main/App";
-import { EventsEmit, EventsOn, LogDebug, LogError } from "wailsjs/runtime/runtime";
+import {
+  EventsEmit,
+  EventsOn,
+  LogDebug,
+  LogError,
+} from "wailsjs/runtime/runtime";
 
+type DBKey = "characters" | "mods" | "tags" | "playlist" | "all";
 
-type DBKey = "characters" | "mods" | "tags" | "playlist" | "all"
+const DBEvent = "DB_EVENT";
+type DBEventData = DBKey[];
 
-const DBEvent = "DB_EVENT"
-type DBEventData = DBKey[]
-
-const subscribeToDbUpdates = (key: DBKey[] | DBKey, callback: () => void, runOnStart: boolean = false): CancelFn => {
-    const runCallbackCatching = () => {
-        try {
-            callback()
-        } catch (e: any) {
-            if (e instanceof Error) {
-                LogError("subscribe listener failed with unhandled Error: " + e.message);
-            } else {
-                LogError("subscribe listener failed with unknown error" + e);
-            }
-        }
+const subscribeToDbUpdates = (
+  key: DBKey[] | DBKey,
+  callback: () => void,
+  runOnStart: boolean = false,
+): CancelFn => {
+  const runCallbackCatching = () => {
+    try {
+      callback();
+    } catch (e: any) {
+      if (e instanceof Error) {
+        LogError(
+          "subscribe listener failed with unhandled Error: " + e.message,
+        );
+      } else {
+        LogError("subscribe listener failed with unknown error" + e);
+      }
     }
+  };
 
-    if (runOnStart) {
-        runCallbackCatching()
+  if (runOnStart) {
+    runCallbackCatching();
+  }
+
+  return EventsOn(DBEvent, (keys: DBEventData) => {
+    LogDebug("received DBEVENT event keys=" + keys);
+    if (typeof key === "string") {
+      if (key === "all" || keys.includes(key)) {
+        runCallbackCatching();
+      }
+    } else {
+      if (key.any((k) => k === "all" || keys.includes(k))) {
+        runCallbackCatching();
+      }
     }
+  });
+};
 
-    return EventsOn(DBEvent, (keys: DBEventData) => {
-        LogDebug("received DBEVENT event keys=" + keys)
-        if (typeof key === 'string') {
-            if (key === "all" || keys.includes(key)) {
-                runCallbackCatching()
-            }
-        } else {
-            if (key.any((k) => k === "all" || keys.includes(k))) {
-                runCallbackCatching()
-            }
-        }
-    })
-}
+export const useDbUpdateListener = (
+  key: DBKey[] | DBKey,
+  callback: () => void,
+) => {
+  useEffect(() => {
+    const cancel = subscribeToDbUpdates(key, callback);
 
-export const useDbUpdateListener = (key: DBKey[] | DBKey, callback: () => void) => {
-    useEffect(() => {
-        const cancel = subscribeToDbUpdates(key, callback)
+    return () => cancel();
+  }, [key]);
+};
 
-        return () => cancel()
-    }, [key])
-
-}
-
-const broadcastCharacterUpdate = () => EventsEmit(DBEvent, ["characters"])
-const broadcastModsUpdate = () => EventsEmit(DBEvent, ["mods"])
-const broadcastTagsUpdate = () => EventsEmit(DBEvent, ["tags"])
-const broadcastPlaylistUpdate = () => EventsEmit(DBEvent, ["playlist"])
+const broadcastCharacterUpdate = () => EventsEmit(DBEvent, ["characters"]);
+const broadcastModsUpdate = () => EventsEmit(DBEvent, ["mods"]);
+const broadcastTagsUpdate = () => EventsEmit(DBEvent, ["tags"]);
+const broadcastPlaylistUpdate = () => EventsEmit(DBEvent, ["playlist"]);
 
 //const broadcastMultiUpdate = (keys: DBKey[]) => EventsEmit(DBEvent, keys)
 const DB = {
-    onValueChangedListener: subscribeToDbUpdates,
-    deleteMod: async (id: number) => {
-        return DeleteModById(id).then(broadcastCharacterUpdate)
-    },
-    enableMod: async (id: number, enabled: boolean) => {
-        return UpdateModEnabledById(enabled, id).then(broadcastModsUpdate)
-    },
-    splitTexture: async (id: number) => {
-        return SplitTexture(id).then(broadcastModsUpdate)
-    },
-    deleteTexture: async (id: number) => {
-        return DeleteTextureById(id).then(broadcastModsUpdate)
-    },
-    enableTexture: async (id: number, enabled: boolean) => {
-        return UpdateTextureEnabledById(id, enabled).then(broadcastModsUpdate)
-    },
-    disableAllMods: async (game: number) => {
-        return UpdateDisableAllModsByGame(game).then(broadcastModsUpdate)
-    },
-    deleteCharacter: async (name: string, id: number, game: number) => {
-        return DeleteCharacter(name, id, game).then(broadcastCharacterUpdate)
-    },
-    selectModById: async (id: number) => {
-        return SelectModById(id)
-    },
-    selectClosestCharacter: async (name: string, game: number) => {
-        return SelectClosestCharacter(name, game)
-    },
-    selectModsByCharacterName: async (name: string, game: number) => {
-        return SelectModsByCharacterName(name, game)
-    },
-    selectCharacterWithModsTagsAndTextures: async (game: number, modFilter: string, characterFilter: string, tagFilter: string) => {
-        return SelectCharacterWithModsTagsAndTextures(game, modFilter, characterFilter, tagFilter)
-    },
-    selectCharactersByGame: async (game: number) => {
-        return SelectCharactersByGame(game)
-    },
-    updateModImages: async (id: number, images: string[]) => {
-        return UpdateModImages(id, images).then(broadcastModsUpdate)
-    },
-    updateModGbId: async (modId: number, gbId: number) => {
-        return UpdateModGbId(modId, gbId).then(broadcastModsUpdate)
-    },
-    renameMod: async (id: number, name: string) => {
-        return RenameMod(id, name).then(broadcastModsUpdate)
-    },
-    renameTexture: async (id: number, name: string) => {
-        return RenameTexture(id, name).then(broadcastModsUpdate)
-    },
-    createCustomCharacter: async (name: string, image: string, element: string | undefined, game: number) => {
-        return CreateCustomCharacter(name, image, element ?? "", game).then(broadcastCharacterUpdate)
-    },
-    insertTag: async (modId: number, name: string) => {
-        return InsertTag(name, modId).then(broadcastTagsUpdate)
-    },
-    insertTagForAllModsByCharacterIds: async (characterIds: number[], tagName: string, game: number) => {
-        return InsertTagForAllModsByCharacterIds(characterIds, tagName, game).then(broadcastTagsUpdate)
-    },
-    selectModsByGbId: async (gbId: number) => {
-        return SelectModsByGbId(gbId)
-    },
-    selectPlaylistWithModsAndTags: async (game: number) => {
-        return SelectPlaylistWithModsAndTags(game)
-    },
-    createPlaylist: async (game: number, name: string) => {
-        return CreatePlaylist(game, name).then(broadcastPlaylistUpdate)
-    },
-    enableMods: (ids: number[], game: number) => {
-        return UpdateModsEnabledFromSlice(ids, game).then(broadcastModsUpdate)
-    },
-    deletePlaylistById: (id: number) => {
-        return DeletePlaylistById(id).then(broadcastPlaylistUpdate)
-    },
-    updatePlaylistName: (id: number, name: string) => {
-        return UpdatePlaylistName(id, name).then(broadcastPlaylistUpdate)
-    },
-    selectTagsByModId: (modId: number) => {
-        return SelectTagsByModId(modId)
-    },
-    deleteTag: (modId: number, name: string) => {
-        return DeleteTag(name, modId).then(broadcastTagsUpdate)
-    }
-}
+  onValueChangedListener: subscribeToDbUpdates,
+  deleteMod: async (id: number) => {
+    return DeleteModById(id).then(broadcastModsUpdate);
+  },
+  enableMod: async (id: number, enabled: boolean) => {
+    return UpdateModEnabledById(enabled, id).then(broadcastModsUpdate);
+  },
+  splitTexture: async (id: number) => {
+    return SplitTexture(id).then(broadcastModsUpdate);
+  },
+  deleteTexture: async (id: number) => {
+    return DeleteTextureById(id).then(broadcastModsUpdate);
+  },
+  enableTexture: async (id: number, enabled: boolean) => {
+    return UpdateTextureEnabledById(id, enabled).then(broadcastModsUpdate);
+  },
+  disableAllMods: async (game: number) => {
+    return UpdateDisableAllModsByGame(game).then(broadcastModsUpdate);
+  },
+  deleteCharacter: async (name: string, id: number, game: number) => {
+    return DeleteCharacter(name, id, game).then(broadcastCharacterUpdate);
+  },
+  selectModById: async (id: number) => {
+    return SelectModById(id);
+  },
+  selectClosestCharacter: async (name: string, game: number) => {
+    return SelectClosestCharacter(name, game);
+  },
+  selectModsByCharacterName: async (name: string, game: number) => {
+    return SelectModsByCharacterName(name, game);
+  },
+  selectCharacterWithModsTagsAndTextures: async (
+    game: number,
+    modFilter: string,
+    characterFilter: string,
+    tagFilter: string,
+  ) => {
+    return SelectCharacterWithModsTagsAndTextures(
+      game,
+      modFilter,
+      characterFilter,
+      tagFilter,
+    );
+  },
+  selectCharactersByGame: async (game: number) => {
+    return SelectCharactersByGame(game);
+  },
+  updateModImages: async (id: number, images: string[]) => {
+    return UpdateModImages(id, images).then(broadcastModsUpdate);
+  },
+  updateModGbId: async (modId: number, gbId: number) => {
+    return UpdateModGbId(modId, gbId).then(broadcastModsUpdate);
+  },
+  renameMod: async (id: number, name: string) => {
+    return RenameMod(id, name).then(broadcastModsUpdate);
+  },
+  renameTexture: async (id: number, name: string) => {
+    return RenameTexture(id, name).then(broadcastModsUpdate);
+  },
+  createCustomCharacter: async (
+    name: string,
+    image: string,
+    element: string | undefined,
+    game: number,
+  ) => {
+    return CreateCustomCharacter(name, image, element ?? "", game).then(
+      broadcastCharacterUpdate,
+    );
+  },
+  insertTag: async (modId: number, name: string) => {
+    return InsertTag(name, modId).then(broadcastTagsUpdate);
+  },
+  insertTagForAllModsByCharacterIds: async (
+    characterIds: number[],
+    tagName: string,
+    game: number,
+  ) => {
+    return InsertTagForAllModsByCharacterIds(characterIds, tagName, game).then(
+      broadcastTagsUpdate,
+    );
+  },
+  selectModsByGbId: async (gbId: number) => {
+    return SelectModsByGbId(gbId);
+  },
+  selectPlaylistWithModsAndTags: async (game: number) => {
+    return SelectPlaylistWithModsAndTags(game);
+  },
+  createPlaylist: async (game: number, name: string) => {
+    return CreatePlaylist(game, name).then(broadcastPlaylistUpdate);
+  },
+  enableMods: (ids: number[], game: number) => {
+    return UpdateModsEnabledFromSlice(ids, game).then(broadcastModsUpdate);
+  },
+  deletePlaylistById: (id: number) => {
+    return DeletePlaylistById(id).then(broadcastPlaylistUpdate);
+  },
+  updatePlaylistName: (id: number, name: string) => {
+    return UpdatePlaylistName(id, name).then(broadcastPlaylistUpdate);
+  },
+  selectTagsByModId: (modId: number) => {
+    return SelectTagsByModId(modId);
+  },
+  deleteTag: (modId: number, name: string) => {
+    return DeleteTag(name, modId).then(broadcastTagsUpdate);
+  },
+};
 
-export default DB
+export default DB;
--- app/frontend/src/screens/mod/ModViewPresenter.ts
+++ app/frontend/src/screens/mod/ModViewPresenter.ts
@@ -9,220 +9,230 @@ import * as Downloader from "../../../wailsjs/go/core/Downloader";
 import * as GbApi from "../../../wailsjs/go/api/GbApi";
 
 export const inDownloadState = (state: State | undefined) => {
-    if (state) {
-        return dlStates.includes(state);
-    }
-    return false;
+  if (state) {
+    return dlStates.includes(state);
+  }
+  return false;
 };
 
 export type ModViewEvent = {
-    changeCharacter: (character: types.Character) => void
-    downloadTexture: (
-        link: string,
-        filename: string,
-        modId: number
-    ) => void
-    downloadMod: (link: string, filename: string) => void,
-    deleteMod: (id: number) => void
-}
+  changeCharacter: (character: types.Character) => void;
+  downloadTexture: (link: string, filename: string, modId: number) => void;
+  downloadMod: (link: string, filename: string) => void;
+  deleteMod: (id: number) => void;
+};
 
 export type ModViewState = {
-    filesWithDownload: ApiFile[],
-    character: types.Character | undefined,
-    mods: types.Mod[]
-    downloaded: types.Mod[]
-    downloadStates: { [link: string]: State },
-    images: string[]
-    events: ModViewEvent,
-    htmlContent: string,
-}
+  filesWithDownload: ApiFile[];
+  character: types.Character | undefined;
+  mods: types.Mod[];
+  downloaded: types.Mod[];
+  downloadStates: { [link: string]: State };
+  images: string[];
+  events: ModViewEvent;
+  htmlContent: string;
+};
 
 export type ApiFile = {
-    dlLink: string,
-    fname: string,
-    dateAdded: number,
-    downloadedMod: types.Mod | undefined,
-    avResults: AVResult[]
-}
-
+  dlLink: string;
+  fname: string;
+  dateAdded: number;
+  downloadedMod: types.Mod | undefined;
+  avResults: AVResult[];
+};
 
 export type AVResult = {
-    result: string;
-    type: string;
-    clean: boolean;
-}
+  result: string;
+  type: string;
+  clean: boolean;
+};
 
 const createAvResults = (file: api.AFile): AVResult[] => {
-    return [
-        { result: file._sClamAvResult, type: "clam av" },
-        { result: file._sAvastAvResult, type: "avast" },
-        { result: file._sAnalysisResult, type: "analysis" }
-    ]
-        .filter((item) => item.result !== undefined)
-        .map(({ result, type }) => {
-            return {
-                result: result!,
-                type: type,
-                clean: result === "clean" || result === "File passed analysis" || result === "ok"
-            }
-        })
-}
+  return [
+    { result: file._sClamAvResult, type: "clam av" },
+    { result: file._sAvastAvResult, type: "avast" },
+    { result: file._sAnalysisResult, type: "analysis" },
+  ]
+    .filter((item) => item.result !== undefined)
+    .map(({ result, type }) => {
+      return {
+        result: result!,
+        type: type,
+        clean:
+          result === "clean" ||
+          result === "File passed analysis" ||
+          result === "ok",
+      };
+    });
+};
 
-const toApiFile = (file: api.AFile, downloaded: types.Mod[]): ApiFile | undefined => {
-    if (file._sDownloadUrl === undefined || file._sFile === undefined) {
-        return undefined
-    }
+const toApiFile = (
+  file: api.AFile,
+  downloaded: types.Mod[],
+): ApiFile | undefined => {
+  if (file._sDownloadUrl === undefined || file._sFile === undefined) {
+    return undefined;
+  }
 
-    return {
-        dlLink: file._sDownloadUrl!,
-        fname: file._sFile!,
-        downloadedMod: downloaded.find((mod) =>
-            file._sFile !== undefined &&
-            mod.gbFileName.toLowerCase() === file._sFile?.toLowerCase()
-        ),
-        avResults: createAvResults(file),
-        dateAdded: file._tsDateAdded ?? 0
-    }
-}
+  return {
+    dlLink: file._sDownloadUrl!,
+    fname: file._sFile!,
+    downloadedMod: downloaded.find(
+      (mod) =>
+        file._sFile !== undefined &&
+        mod.gbFileName.toLowerCase() === file._sFile?.toLowerCase(),
+    ),
+    avResults: createAvResults(file),
+    dateAdded: file._tsDateAdded ?? 0,
+  };
+};
 
-export const useModViewPresenter = (id: string | undefined, dataApi: DataApi): ModViewState => {
-    const running = useDownloadStore(useShallow((state) => state.running));
-    const [character, setCharacter] = useState<types.Character | undefined>(
-        undefined
-    );
-    const game = useStateProducer<number | undefined>(undefined, (update) => {
-        dataApi.game().then(update)
-    })
-    const content = useStateProducer<api.ModPageResponse | undefined>(
-        undefined,
-        async (update) => {
-            const page = await GbApi.ModPage(Number(id));
-            update(page);
-        },
-        [id]
-    );
+export const useModViewPresenter = (
+  id: string | undefined,
+  dataApi: DataApi,
+): ModViewState => {
+  const running = useDownloadStore(useShallow((state) => state.running));
+  const [character, setCharacter] = useState<types.Character | undefined>(
+    undefined,
+  );
+  const game = useStateProducer<number | undefined>(undefined, (update) => {
+    dataApi.game().then(update);
+  });
+  const content = useStateProducer<api.ModPageResponse | undefined>(
+    undefined,
+    async (update) => {
+      const page = await GbApi.ModPage(Number(id));
+      update(page);
+    },
+    [id],
+  );
 
-
-    const mods = useStateProducer<types.Mod[]>(
-        [],
-        async (update, onDispose) => {
-            if (character) {
-                const cancel = DB.onValueChangedListener("mods", () => {
-                    DB.selectModsByCharacterName(character.name, character.game).then(update);
-                }, true)
-
-                onDispose(cancel)
-            }
-        },
-        [character]
-    );
-
-    const downloaded = useStateProducer<types.Mod[]>(
-        [],
-        async (update, onDispose) => {
-            const cancel = DB.onValueChangedListener("mods", async () => {
-                const mods = await DB.selectModsByGbId(Number(id))
-                const filtered = mods.filter(m => m.characterId === character?.id)
-                update(filtered)
-            }, true)
-
-            onDispose(cancel)
-        },
-        [running, id, character]
-    );
-
-
-    useEffect(() => {
-        let cname = content?._aCategory?._sName;
-        let done = false
-        if (cname === undefined || dataApi === undefined || game === undefined) return
-
-        let names = [cname]
-        if (game === Game.ZZZ) {
-            names = cname.split(" ")
-        }
-
-        for (const name of names) {
-            DB.selectClosestCharacter(name, game)
-                .then((result) => {
-                    if (!done && character === undefined) {
-                        setCharacter(result)
-                    }
-                })
-        }
-
-        return () => {
-            done = true
-        }
-    }, [content, dataApi, game]);
-
-    const downloadStates: { [link: string]: State } = useDownloadStore(
-        useShallow((state) =>
-            Object.fromEntries(
-                Object.entries(state.downloads)
-                    .map(([link, download]) => [link, download.state])
-            )
-        ));
-
-
-    const images = useMemo(() => {
-        return (
-            content?._aPreviewMedia?._aImages
-                ?.map((image) => `${image._sBaseUrl}/${image._sFile}`)
-                ?.filter((url) => url !== undefined)
-            ?? []
+  const mods = useStateProducer<types.Mod[]>(
+    [],
+    async (update, onDispose) => {
+      if (character) {
+        const cancel = DB.onValueChangedListener(
+          ["mods", "characters"],
+          () => {
+            DB.selectModsByCharacterName(character.name, character.game).then(
+              update,
+            );
+          },
+          true,
         );
-    }, [content]);
 
-    const filesWithDownload = useMemo(() => {
-        return content
-            ?._aFiles
-            ?.map((file) => toApiFile(file, downloaded))
-            .filter((file) => file !== undefined)
-            ?? []
-    }, [content, downloaded])
+        onDispose(cancel);
+      }
+    },
+    [character],
+  );
 
-    return {
-        filesWithDownload: filesWithDownload,
-        downloaded: downloaded,
-        mods: mods,
-        images: images,
-        downloadStates: downloadStates,
-        character: character,
-        htmlContent: content?._sText ?? "",
-        events: {
-            changeCharacter: (c) => setCharacter(c),
-            downloadMod: (link: string, filename: string) => {
-                if (character !== undefined && content?._idRow !== undefined) {
-                    Downloader.Download(
-                        link,
-                        filename,
-                        character.name,
-                        character.id,
-                        character.game,
-                        content?._idRow,
-                        images
-                    );
-                }
-            },
-            downloadTexture: (
-                link: string,
-                filename: string,
-                modId: number
-            ) => {
-                if (character !== undefined && content?._idRow !== undefined) {
-                    Downloader.DownloadTexture(
-                        link,
-                        filename,
-                        modId,
-                        content?._idRow,
-                        images
-                    );
-                }
-            },
-            deleteMod: (id: number) => {
-                DB.deleteMod(id);
-            }
-        }
+  const downloaded = useStateProducer<types.Mod[]>(
+    [],
+    async (update, onDispose) => {
+      const cancel = DB.onValueChangedListener(
+        ["mods", "characters"],
+        async () => {
+          const mods = await DB.selectModsByGbId(Number(id));
+          const filtered = mods.filter((m) => m.characterId === character?.id);
+          update(filtered);
+        },
+        true,
+      );
+
+      onDispose(cancel);
+    },
+    [running, id, character],
+  );
+
+  useEffect(() => {
+    let cname = content?._aCategory?._sName;
+    let done = false;
+    if (cname === undefined || dataApi === undefined || game === undefined)
+      return;
+
+    let names = [cname];
+    if (game === Game.ZZZ) {
+      names = cname.split(" ");
     }
-}
+
+    for (const name of names) {
+      DB.selectClosestCharacter(name, game).then((result) => {
+        if (!done && character === undefined) {
+          setCharacter(result);
+        }
+      });
+    }
+
+    return () => {
+      done = true;
+    };
+  }, [content, dataApi, game]);
+
+  const downloadStates: { [link: string]: State } = useDownloadStore(
+    useShallow((state) =>
+      Object.fromEntries(
+        Object.entries(state.downloads).map(([link, download]) => [
+          link,
+          download.state,
+        ]),
+      ),
+    ),
+  );
+
+  const images = useMemo(() => {
+    return (
+      content?._aPreviewMedia?._aImages
+        ?.map((image) => `${image._sBaseUrl}/${image._sFile}`)
+        ?.filter((url) => url !== undefined) ?? []
+    );
+  }, [content]);
+
+  const filesWithDownload = useMemo(() => {
+    return (
+      content?._aFiles
+        ?.map((file) => toApiFile(file, downloaded))
+        .filter((file) => file !== undefined) ?? []
+    );
+  }, [content, downloaded]);
+
+  return {
+    filesWithDownload: filesWithDownload,
+    downloaded: downloaded,
+    mods: mods,
+    images: images,
+    downloadStates: downloadStates,
+    character: character,
+    htmlContent: content?._sText ?? "",
+    events: {
+      changeCharacter: (c) => setCharacter(c),
+      downloadMod: (link: string, filename: string) => {
+        if (character !== undefined && content?._idRow !== undefined) {
+          Downloader.Download(
+            link,
+            filename,
+            character.name,
+            character.id,
+            character.game,
+            content?._idRow,
+            images,
+          );
+        }
+      },
+      downloadTexture: (link: string, filename: string, modId: number) => {
+        if (character !== undefined && content?._idRow !== undefined) {
+          Downloader.DownloadTexture(
+            link,
+            filename,
+            modId,
+            content?._idRow,
+            images,
+          );
+        }
+      },
+      deleteMod: (id: number) => {
+        DB.deleteMod(id);
+      },
+    },
+  };
+};
